d5f1f2cdee6997b7be57c019a96dcffce4ca70f4
lucas-silva <lucas.silva3218@etec.sp.gov.br>
2025-12-12 14:30:54 -0300

    imagens

--- src/load/upseller_template.py
+++ src/load/upseller_template.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 import pandas as pd
@@ -7,24 +8,18 @@ upseller = pd.read_excel(arquivo)
 produtos = pd.read_excel(
     "/home/lucas-silva/auto_shopee/planilhas/outputs/final_urls_cloudinary.xlsx"
 )
+
 upseller_colunas = upseller.columns.tolist()
 
 
-def criar_linha(item, colunas_bling):
-    linha = {col: "" for col in colunas_bling}
+def criar_linha(item, colunas_upseller):
+    linha = {col: "" for col in colunas_upseller}
 
     mapa = {
-        "Nome do Produto*": item.get("Base", ""),
-        "SKU Principal": item.get("SKU_Pai", ""),
+        "Nome do Produto*": item.get("Descrição", ""),
+        "SKU Principal": item.get("Sku", ""),
         "Descrição*": item.get("Descrição", ""),
         "Categoria ID": "101199",
-        "Nome Variante1": "Variação",
-        "Opção por Variante1": item.get("Variante", ""),
-        "Imagem por Variante": item.get("Url_Imagem1.0", "")
-        or item.get("Url_Imagem2.0")
-        or item.get("Url_Imagem3.0")
-        or item.get("Url Imagem"),
-        "SKU": item.get("Sku", ""),
         "Preço*": item.get("Preço Final", ""),
         "Quantidade*": "0",
         "GTIN": item.get("Código de Barras", ""),
@@ -33,7 +28,7 @@ def criar_linha(item, colunas_bling):
         "Item da Imagem2": item.get("Url_Imagem3.0", ""),
         "Item da Imagem4": item.get("Url Imagem", ""),
         "Item da Imagem3": "https://res.cloudinary.com/dbpq32fiq/image/upload/v1764623485/46ba78f5-9d01-49fb-a96f-f00aead7f851_wgbwkj.png",
-        "Peso (kg)*": item.get("Peso", "1"),
+        "Peso (kg)*": item.get("Peso", ""),
         "Comprimento (cm)": item.get("Comprimento", ""),
         "Largura (cm)": item.get("Largura", ""),
         "Altura (cm)": item.get("Altura", ""),
@@ -43,21 +38,26 @@ def criar_linha(item, colunas_bling):
     return linha
 
 
-linhas_bling = [criar_linha(row, upseller_colunas) for _, row in produtos.iterrows()]
-bling_df = pd.DataFrame(linhas_bling, columns=upseller_colunas)
+linhas_upseller = [criar_linha(row, upseller_colunas) for _, row in produtos.iterrows()]
+upseller_df = pd.DataFrame(linhas_upseller, columns=upseller_colunas)
 
 
-# Peso inteiro
-bling_df["Peso (kg)*"] = (
-    pd.to_numeric(bling_df["Peso (kg)*"], errors="coerce").fillna(1).round(0).astype("Int64")
+upseller_df["Peso (kg)*"] = (
+    pd.to_numeric(upseller_df["Peso (kg)*"], errors="coerce")
+    .fillna(1)
+    .apply(lambda x: math.ceil(x))  # arredonda pra cima
+    .astype("Int64")
 )
+
 
 # Dimensões inteiras (Comprimento, Largura, Altura)
 for col in ["Comprimento (cm)", "Largura (cm)", "Altura (cm)"]:
-    bling_df[col] = pd.to_numeric(bling_df[col], errors="coerce").fillna(0).round(0).astype("Int64")
+    upseller_df[col] = (
+        pd.to_numeric(upseller_df[col], errors="coerce").fillna(0).round(0).astype("Int64")
+    )
 
 # Preço permanece com decimais (caso você já tenha ajustado antes)
-bling_df["Preço*"] = pd.to_numeric(bling_df["Preço*"], errors="coerce").round(2)
+upseller_df["Preço*"] = pd.to_numeric(upseller_df["Preço*"], errors="coerce").round(2)
 
 # ---------------------------------------
 # Mantém o restante igual
@@ -65,8 +65,8 @@ bling_df["Preço*"] = pd.to_numeric(bling_df["Preço*"], errors="coerce").round(
 
 if os.path.exists(arquivo):
     df_existente = pd.read_excel(arquivo)
-    df_final = pd.concat([df_existente, bling_df], ignore_index=True)
+    df_final = pd.concat([df_existente, upseller_df], ignore_index=True)
 else:
-    df_final = bling_df
+    df_final = upseller_df
 
 df_final.to_excel("upseller.xlsx", index=False)
