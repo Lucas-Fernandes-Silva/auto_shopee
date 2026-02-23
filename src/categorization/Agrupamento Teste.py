0cdd0d5f2b601d5107af02d50a01c4180055df4f
lucas-silva <lucas.silva3218@etec.sp.gov.br>
2026-02-23 13:51:49 -0300

    orgnizar bagunça

--- src/categorization/Agrupamento Teste.py
+++ src/categorization/Agrupamento Teste.py
@@ -1,7 +1,40 @@
+import pandas as pd
+
+df = pd.read_excel("/home/lucas-silva/auto_shopee/planilhas/outputs/Produtos_Classificados_Por_Dominio.xlsx")
+
 df["Produto_Base_ID"] = (
     df["grupo_id"].astype(str)
     + "|"
     + df["Marca"].str.upper().fillna("SEM_MARCA")
 )
+
+df["Nome_Produto_Base"] = (
+    "Disjuntor DIN Curva C "
+    + df["Marca"].str.upper()
+)
+
+df["Chave_Variacao"] = (
+    df["Polos"].fillna("NA").astype(str)
+    + "|"
+    + df["Amperagem"].fillna("NA").astype(str)
+)
+
+df["Nome_Variacao"] = (
+    df["Polos"].fillna("")
+    + " "
+    + df["Amperagem"].astype(str)
+    + "A"
+)
+
+df[
+    df["grupo_id"] == 436
+][
+    [
+        "Descricao_Limpa",
+        "Marca",
+        "Nome_Produto_Base",
+        "Nome_Variacao"
+    ]
+]
 
 
